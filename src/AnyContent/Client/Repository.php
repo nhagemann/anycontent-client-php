a0b517e4c70ef98376b62b1b6b57e799a3d322eb
Nils Hagemann <mail@nilshagemann.de>
2013-12-29 17:26:09 +0100

    added methods of client to retrieve records to the client dependent repository object

--- src/AnyContent/Client/Repository.php
+++ src/AnyContent/Client/Repository.php
@@ -14,6 +14,9 @@ class Repository
     /** @var  Client */
     protected $client;
 
+    protected $contentTypeName = '';
+    protected $contentTypeDefinition = null;
+
 
     public function __construct($client)
     {
@@ -48,5 +51,38 @@ class Repository
     public function hasContentType($contentTypeName)
     {
         return array_key_exists($contentTypeName, $this->client->getContentTypeList());
+    }
+
+
+    public function selectContentType($contentTypeName)
+    {
+        if ($this->contentTypeName != $contentTypeName)
+        {
+            $this->contentTypeName       = $contentTypeName;
+            $this->contentTypeDefinition = $this->getContentTypeDefinition($contentTypeName);
+        }
+    }
+
+
+    public function getRecord($id, $workspace = 'default', $clippingName = 'default', $language = 'none', $timeshift = 0)
+    {
+        if ($this->contentTypeDefinition)
+        {
+            return $this->client->getRecord($this->contentTypeDefinition, $id, $workspace, $clippingName, $language, $timeshift);
+        }
+
+        return false;
+
+    }
+
+
+    public function getRecords($workspace = 'default', $clippingName = 'default', $language = 'none', $order = 'id', $properties = array(), $limit = null, $page = 1, $timeshift = 0)
+    {
+        if ($this->contentTypeDefinition)
+        {
+            return $this->client->getRecords($this->contentTypeDefinition, $workspace, $clippingName, $language, $order, $properties, $limit, $page, $timeshift);
+        }
+
+        return false;
     }
 }
