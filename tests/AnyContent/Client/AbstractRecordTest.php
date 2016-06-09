eeeec2a4bdb44c559126f4496a46807fefdb08e4
Nils Hagemann <mail@nilshagemann.de>
2016-06-09 16:10:10 +0200

    bugfix logging logging guzzle exceptions adjusted getProperty method to apply default on empty strings and null values

---
+++ tests/AnyContent/Client/AbstractRecordTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace AnyContent\Client;
+
+use AnyContent\Connection\Configuration\ContentArchiveConfiguration;
+use AnyContent\Connection\ContentArchiveReadWriteConnection;
+use KVMLogger\KVMLoggerFactory;
+use Symfony\Component\Filesystem\Filesystem;
+
+class AbstractRecordTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  ContentArchiveReadWriteConnection */
+    public $connection;
+
+    /** @var  Repository */
+    public $repository;
+
+
+    public static function setUpBeforeClass()
+    {
+        $target = __DIR__ . '/../../../tmp/ExampleContentArchive';
+        $source = __DIR__ . '/../../resources/ContentArchiveExample2';
+
+        $fs = new Filesystem();
+
+        if (file_exists($target))
+        {
+            $fs->remove($target);
+        }
+
+        $fs->mirror($source, $target);
+
+        KVMLoggerFactory::createWithKLogger(__DIR__.'/../../../tmp');
+
+    }
+
+
+
+    public function setUp()
+    {
+        $target = __DIR__ . '/../../../tmp/ExampleContentArchive';
+
+        $configuration = new ContentArchiveConfiguration();
+
+        $configuration->setContentArchiveFolder($target);
+
+        $connection = $configuration->createReadWriteConnection();
+
+        $this->connection = $connection;
+
+        $this->repository = new Repository('phpunit',$this->connection);
+
+    }
+
+
+    public function testSaveRecords()
+    {
+        $this->repository->selectContentType('example01');
+
+        for ($i = 1; $i <= 5; $i++)
+        {
+            $record = $this->repository->createRecord('New Record ' . $i);
+
+            $this->assertEquals('DEFAULT',$record->getProperty('article','DEFAULT'));
+
+            $record->setProperty('article','');
+            $id = $this->repository->saveRecord($record);
+            $this->assertEquals($i, $id);
+        }
+
+        $record = $this->repository->getRecord(1);
+        $this->assertEquals('DEFAULT',$record->getProperty('article','DEFAULT'));
+
+        $record->setProperty('article',0);
+        $id = $this->repository->saveRecord($record);
+
+        $record = $this->repository->getRecord(1);
+        $this->assertEquals('0',$record->getProperty('article','DEFAULT'));
+
+        $record->setProperty('article',null);
+        $id = $this->repository->saveRecord($record);
+
+        $record = $this->repository->getRecord(1);
+        $this->assertEquals('DEFAULT',$record->getProperty('article','DEFAULT'));
+
+    }
+}
